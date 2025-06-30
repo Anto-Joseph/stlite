60c69804c0f6b4baccc48a6b5cb6f845cfbe17e8
Anto Joseph <94463056+Anto-Joseph@users.noreply.github.com>
2025-06-30 22:53:47 +0000

    udated the codes

--- pages/3_ActivitySheet.py
+++ pages/3_ActivitySheet.py
@@ -3,7 +3,10 @@ import datetime
 import streamlit_authenticator as stauth
 import pandas as pd
 import sqlite3
+
+
 st.title('Daily Work Sheet')
+AddedByUser = st.session_state.get("name")
 # --- DATABASE CONNECTION ---
 
 con = sqlite3.connect("database-1.db")
@@ -38,12 +41,12 @@ if st.button("Add New", icon="➕") :
 # ----- Table Data ---- 
 
 df_empList = pd.read_sql_query("SELECT EmpID,EmpName,EmpStatus,EmpDesignation from EMP", con)
-df_actList = pd.read_sql_query("SELECT * from TASK", con)
+df_taskList = pd.read_sql_query("SELECT * from TASK", con)
 df_AllEmpActList = pd.read_sql_query(f"SELECT * from View_EmpTaskSheet WHERE TaskDate = '{A_date}'", con)
-df_EmpAct = df_AllEmpActList.filter(['EmpName','TaskDescription', 'Comments', 'EnteredBy'], axis=1)
+df_EmpAct = df_AllEmpActList.filter(['TaskEmpID','EmpName','TaskDescription', 'Comments', 'EnteredBy'], axis=1)
 EmpList = df_empList["EmpName"]
-ActList = df_actList["TaskDescription"].to_list()
-print(ActList)
+TaskList = df_taskList["TaskDescription"].to_list()
+
 
 # ----- Table -----
 
@@ -58,11 +61,37 @@ edited_df = st.data_editor(
                                                     ),
       "TaskDescription" : st.column_config.SelectboxColumn("Activity",
                                                      width = 'large', 
-                                                     options= ActList,
+                                                     options= TaskList,
                                                      required=True,
                                                    ),
       "Comments" : st.column_config.Column("Comments", width = 'large',),
+      "EnteredBy" : st.column_config.TextColumn("Widgets", default= st.session_state.get("name")),
+      "TaskDate" : st.column_config.DateColumn(default= A_date ),
+
 
     },
 
+    column_order=("EmpName", "TaskDescription","Comments","EnteredBy"),
+
 )
+
+#df_taskList
+#df_empList
+#edited_df
+
+df_x = pd.merge(edited_df, df_taskList, how="inner", on=["TaskDescription"])
+df_x = pd.merge(df_x, df_empList, how="inner", on=["EmpName"])
+df_x = df_x.filter(['EmpID','TaskID','TaskDate','Comments','EnteredBy'])
+
+
+#df_x = df_x.filter(['TaskEmpID','EmpName','TaskDescription','Comments','EnteredBy'])
+df_x
+df_EmpAct
+
+#compare identical dataframes
+#x = df_x.compare(df_EmpAct)
+
+
+
+#df.to_sql(name='users', con=engine)
+#st.session_state.get("name")
