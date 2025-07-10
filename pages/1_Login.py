d98500d812b47e4adc8174011a01d3d3d871712b
Anto Joseph <94463056+Anto-Joseph@users.noreply.github.com>
2025-07-10 00:15:10 +0000

    DailyTimesheet-upload template done- DB connect remaining

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+
+st.set_page_config(layout="wide")
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)    
 authenticator = stauth.Authenticate(
--- pages/2_Timesheet.py
+++ pages/2_Timesheet.py
@@ -2,10 +2,15 @@ import streamlit as st
 import datetime
 import streamlit_authenticator as stauth
 import pandas as pd
+from openpyxl import load_workbook
+import numpy as np
+
+Xl_SkipColumns = []
+
 
 st.title('Daily Timesheet')
 st.write(f'Entered By : *{st.session_state.get("name")}*')
-
+file_df = []
 
 T_date = st.date_input("Select Input date")
 
@@ -16,8 +21,39 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
 
     try :
-        file_df = pd.read_excel(uploaded_file, sheet_name=0)
-        file_df
+        file_df = load_workbook(uploaded_file)
+
     except Exception as e :
         st.warning("Opps wrong file format")
         st.write(e)
+
+if file_df != []:
+    Xl_Sheet = st.selectbox(
+                                "Select your sheet",
+                                file_df.sheetnames,
+                                index=None,
+                                placeholder="Select Sheet...",
+                            )
+    Xl_FormatDataType = st.selectbox(
+                                "Load Data As",
+                                ("By Sheet","By Table"),
+                                index=None,
+                                placeholder="Format as...",
+                            )
+    if Xl_FormatDataType == "By Sheet" :
+        Xl_selectedSheetDF = pd.DataFrame(file_df[Xl_Sheet].values)
+        Xl_SkipRowNo =  st.number_input("Header Row Number",step = 1,min_value  = 0)
+        headerRow = Xl_selectedSheetDF.loc[Xl_SkipRowNo].to_frame()
+        headerRow['ColNum'] = headerRow.index
+        headerRow = headerRow.dropna()
+        headerRow.T
+        Xl_selectedSheetDF = Xl_selectedSheetDF.iloc[0:][headerRow['ColNum']]
+        Xl_selectedSheetDF.columns = Xl_selectedSheetDF.iloc[Xl_SkipRowNo]
+        Xl_selectedSheetDF =  Xl_selectedSheetDF.iloc[Xl_SkipRowNo:]
+        Xl_selectedSheetDF
+
+
+
+
+
+
